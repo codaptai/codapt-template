7ea145748fb4d5b48a17706e3a8f56f18b9db197
Trevor Keith <tk@tkeith.com>
2024-05-09 12:59:36 -0400

    invalidate all queries with every mutation

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -9,7 +9,26 @@ import { getUrl, transformer } from "./shared";
 
 import { AppRouter } from "@/server/api/root";
 
-export const api = createTRPCReact<AppRouter>();
+export const api = createTRPCReact<AppRouter>({
+  overrides: {
+    useMutation: {
+      /**
+       * This function is called whenever a `.useMutation` succeeds
+       **/
+      async onSuccess(opts) {
+        /**
+         * @note that order here matters:
+         * The order here allows route changes in `onSuccess` without
+         * having a flash of content change whilst redirecting.
+         **/
+        // Calls the `onSuccess` defined in the `useQuery()`-options:
+        await opts.originalFn();
+        // Invalidate all queries in the react-query cache:
+        await opts.queryClient.invalidateQueries();
+      },
+    },
+  },
+});
 
 export function TRPCReactProvider(props: {
   children: React.ReactNode;
